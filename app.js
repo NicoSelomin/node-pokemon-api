2a5f2956b8e1f937e45d33b1e986db76fdfc6703
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2025-04-29 04:23:56 -0400

    feat : setup basic express server

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("JE suis ok");
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res)=> res.send("Hello express !"));
+
+app.listen(port, ()=> console.log(`Notre application node est démarré sur: http://localhost:${port}`));
