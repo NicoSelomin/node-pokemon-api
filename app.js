5dad0c4774b17c3759ff0a3cb8c5a8975cb1087f
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2025-04-30 18:39:28 -0400

    feat: add mock data and routes to list and get a Pokémon by ID

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const pokemons = require('./mock-pokemon');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res)=> res.send("Hello express !"));
+app.get('/', (req,res)=> res.send("Hello dans mon app express !"));
+app.get('/api/pokemons', (req,res)=>{
+    res.send(`Il y a ${pokemons.length} pokemons dans le pokédex pour le moment.`)
+});
+app.get('/api/pokemons/:id', (req,res)=>{
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon=>pokemon.id === id);
+    res.json(pokemon)
+});
 
 app.listen(port, ()=> console.log(`Notre application node est démarré sur: http://localhost:${port}`));
