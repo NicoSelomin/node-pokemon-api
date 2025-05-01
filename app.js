924c40754ddf69baf049725d84548391ecb4ef2c
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2025-04-30 20:18:00 -0400

    feat: add helper.js with a success function to return message and data correctly and create api/pokemons end point to return pokemon list

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {success} = require('./helper')
 const pokemons = require('./mock-pokemon');
 
 const app = express();
@@ -6,12 +7,14 @@ const port = 3000;
 
 app.get('/', (req,res)=> res.send("Hello dans mon app express !"));
 app.get('/api/pokemons', (req,res)=>{
-    res.send(`Il y a ${pokemons.length} pokemons dans le pokédex pour le moment.`)
+    const message = "La liste des pokemons a été bien récupérée."
+    res.json(success(message,pokemons))
 });
 app.get('/api/pokemons/:id', (req,res)=>{
     const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon=>pokemon.id === id);
-    res.json(pokemon)
+    const message = "Un pokemon a été bien trouvé";
+    res.json(success(message,pokemon))
 });
 
 app.listen(port, ()=> console.log(`Notre application node est démarré sur: http://localhost:${port}`));
